1f4e752243dcf5dfdb016edcca56663efb43c51f
CR4ZYM4D <madhavdixit482@gmail.com>
2025-11-24 16:04:00 +0530

    Retrofit API Handler

---
+++ app/src/main/java/com/example/kraftor/backend/network/AuthInterceptor.kt
@@ -0,0 +1,9 @@
+package com.example.kraftor.backend.network
+
+import okhttp3.Interceptor
+
+class AuthInterceptor(private val tokenStorage: TokenStorage): Interceptor{
+
+
+
+}
---
+++ app/src/main/java/com/example/kraftor/backend/network/RetrofitProvider.kt
@@ -0,0 +1,60 @@
+package com.example.kraftor.backend.network
+
+/**
+ * Object to build the Base URL for the REST API Calls
+ * @param -> context: Context
+ * @return -> ApiService class object
+ * */
+
+import android.content.Context
+
+// HTTP requests
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+// making API calls
+import retrofit2.Retrofit
+
+//De-Serialization of JSONs
+import com.squareup.moshi.Moshi
+import retrofit2.converter.moshi.MoshiConverterFactory
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+import kotlin.jvm.java
+
+object RetrofitProvider {
+
+    private const val BASE_URL = "https://rv6p8wji3k.execute-api.ap-south-1.amazonaws.com/kode-kalesh"
+
+    fun createAPI(context: Context): ApiService{
+
+        // get user token
+        val tokenStorage = TokenStorage(context.applicationContext)
+
+        // create auth token interceptor
+        val authInterceptor = AuthInterceptor(tokenStorage)
+
+        // build HTTP Client
+        val client = OkHttpClient.Builder()
+            .addInterceptor(authInterceptor) // add auth token to request header
+            .connectTimeout(30, TimeUnit.SECONDS) // connection timeout limit
+            .readTimeout(60, TimeUnit.SECONDS) // read timeout limit b/w packets
+            .build()
+
+        // build moshi object for JSON to dataclass conversion
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        // build retrofit object for making/handling API calls
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+        return retrofit.create(ApiService :: class.java)
+
+    }
+
+}
